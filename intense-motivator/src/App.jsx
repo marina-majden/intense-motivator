e1078d1bef81dc4b37b4217cbba9811f0f3ea926
Marina Majdenić <marina.majden@gmail.com>
2024-02-04 22:14:10 +0100

    add quote generator component

---
+++ intense-motivator/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Quote from "./components/Quote";
+
+function App() {
+  return (
+    <>
+      <h1>Intense Motivator</h1>
+      <div className="card">
+        <h2>Get Motivated</h2>
+        <Quote />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ intense-motivator/src/components/Quote.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const url = "https://api.quotable.io/random";
+
+function Quote() {
+  {
+    /* HR: kreiramo tri varijable koje će pohraniti podatke o citatu i autoru te varijablu koja će pohraniti podatke preuzete s URL-a. */
+  }
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  {
+    /* HR: kreiramo asinkronu funkciju koja će preuzeti podatke o citatima s navedenog URL-a i spremiti podatke u data varijablu koju smo stvorili s tim ciljem; data je objekt koji treba sadržavati "content", tj. tekst citata i "author", tj. ime autora citata. */
+  }
+  useEffect(() => {
+    getQuotesData();
+  }, []);
+  async function getQuotesData() {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    setQuote(data.content);
+    setAuthor(data.author);
+  }
+  {
+    /* Kreiramo uvjetovanu tvrdnju koja će ispisati poruku korisniku ako dođe do greške te sadržaj ne bude uspješno preuzet s URL-a; ako podaci ipak postoje (uspješno su preuzeti), tada se nastavlja kod koji vraća markup, tj. opisuje sadržaj komponente kako će ga browser učitati i prikazati korisniku. */
+  }
+  if (!quote || !author) {
+    ("Can't get data at the moment. Please try again later.");
+  }
+  return (
+    <>
+      <div>
+        <h1>Quote of the day</h1>
+        <p>{quote}</p>
+        <p>{author}</p>
+      </div>
+      <button onClick={getQuotesData}>Get Quote</button>
+    </>
+  );
+}
+
+export default Quote;
